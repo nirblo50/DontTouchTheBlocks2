e3215d9ea0b1fed3f894bb98442963fcd4b20637
Nir Balouka <nirblo50@gmail.com>
2016-12-04 12:27:20 +0200

    8 working blocks without leaderboard

---
+++ core/src/com/mygdx/game/Background.java
@@ -0,0 +1,39 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+/**
+ * Created by nirbl on 27/11/2016.
+ */
+
+public class Background
+{
+    private Texture texture;
+    private Batch batch;
+
+    public Background()
+    {
+        texture = new Texture(Gdx.files.internal("background2.png"));
+        batch = new SpriteBatch();
+
+    }
+
+    public void drawBackground()
+    {
+        batch.begin();
+        batch.draw(texture, -10, 0, Gdx.graphics.getWidth()+20, Gdx.graphics.getHeight());
+        batch.end();
+
+    }
+
+    public Texture getTexture() {
+        return texture;
+    }
+
+    public Batch getBatch() {
+        return batch;
+    }
+}
